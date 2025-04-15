74b8ca9247bd17e2a430f08a120d905cb5685462
YASHVANTH P S <166122718+Yash-Raj-96@users.noreply.github.com>
2025-04-15 23:30:09 +0530

    Add files via upload

---
+++ app/blog/page.js
@@ -0,0 +1,22 @@
+// @flow strict
+
+import { personalData } from "@/utils/data/personal-data";
+
+async function getBlogs() {
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = await res.json();
+  return data;
+};
+
+async function page() {
+  const blogs = await getBlogs();
+
+  
+};
+
+export default page;
